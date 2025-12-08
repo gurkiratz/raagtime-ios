c378ad4b5737752b2b98074c39744c58ce771b7f
gurkiratz <singh.gurkirat2707@gmail.com>
2025-12-08 12:45:14 -0500

    Initial Commit

---
+++ RaagTime/RaagTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RaagTimeApp.swift
+//  RaagTime
+//
+//  Created by Gurkirat Singh on 2025-12-08.
+//
+
+import SwiftUI
+
+@main
+struct RaagTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
